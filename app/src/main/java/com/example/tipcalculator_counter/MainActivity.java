80e52e2b340d1b0729b7e957e884d2ba0153c3dd
Hollande <hollandepowell@gmail.com>
2020-03-11 17:24:59 -0400

    Layout WIP

--- app/src/main/java/com/example/tipcalculator_counter/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_counter/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
     }
 
     private void showErrorAlert(String errorMessage, final int fieldId) {
